80162ef81159ee6de7d97462788eb1ec6c2bab86
huidongchen <hd7chen@gmail.com>
2019-07-15 18:15:04 -0400

    add Scasat

---
+++ Real_Data/Cusanovich_2018_subset/run_methods/Scasat/count_reads_peaks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#BSUB -J count_reads[1-12178]
+#BSUB -o count_reads_peaks.out
+#BSUB -e count_reads_peaks.err
+#BSUB -We 5
+#BSUB -q vshort
+
+source activate ATACseq_preprocess
+bampath=../../input/sc-bams_nodup/
+dirlist=(`ls $bampath*.bam`)
+# echo ${dirlist[$LSB_JOBINDEX-1]}
+mkdir -p count_reads_peaks_output
+echo ./count_reads_peaks_output/$(basename ${dirlist[$LSB_JOBINDEX-1]}).peaks.txt
+bedtools coverage -a ../../input/combined.sorted.merged.bed -b ${dirlist[$LSB_JOBINDEX-1]} > ./count_reads_peaks_output/$(basename ${dirlist[$LSB_JOBINDEX-1]}).peaks.txt
